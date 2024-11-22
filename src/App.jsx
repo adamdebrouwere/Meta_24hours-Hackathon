95630189016a60bdd0f720684587d8fa39e16599
adamdebrouwere <adamdebrouwere@hotmail.com>
2024-11-21 19:56:25 -0600

    add: display post data

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
+import fs from 'fs';
+import path from 'path';
+import { useState, useEffect} from 'react';
+
+const postFilePath = './postData.json';
+
+
 
 function App() {
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('postData.json').then(response => response.json()).then(data => {
+      setPosts(data);
+      console.log("postdata:", data)
+    }).catch(error => {
+      console.error("Error fetching file:", error)
+    })
+  }, [])
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+    <h1>Posts</h1>
+    <ul>
+      {posts.map((post, index) => (
+        <li key={index}>
+          <h2>{post.title}</h2>
+          <p>{post.content}</p>
+          <small>By {post.author} on {post.date}</small>
+        </li>
+      ))}
+    </ul>
+  </div>
   );
 }
 
