7b2e8761d92a460d1e0dfc890d55f95d5a57dafa
Laura Huang <lauraqifei@gmail.com>
2024-11-22 00:25:12 -0500

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
 import './App.scss';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import postData from '/src/data/postData.json';
 import { sortPosts } from '/src/utils/sortPosts';
 
-
-
-
 function App() {
-  const [posts, setPosts] = useState(postData)
+  const [posts, setPosts] = useState(postData);
 
   useEffect(() => {
-
-
     const test = sortPosts("I've been stressed and want to relax", posts);
     setPosts(test);
-  }, [])
+  }, []);
 
   return (
     <div>
-    <h1>Posts</h1>
-    <ul>
-      {posts.map((post, index) => (
-        <li key={index}>
-          <p>{post.content}</p>
-          <small>By {post.author} on {post.date}</small>
-        </li>
-      ))}
-    </ul>
-  </div>
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>
+            <p>{post.content}</p>
+            <small>
+              By {post.author} on {post.date}
+            </small>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
